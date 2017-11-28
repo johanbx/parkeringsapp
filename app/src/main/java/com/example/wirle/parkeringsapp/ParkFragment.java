fffca6679ca1e4156669406880cf9a47b96c62e0
johanbx <johan.borgenstierna@outlook.com>
2017-11-28 16:36:01 +0100

    implementerar google mapps i parkfragment, fungerar ej i emulatorn

--- app/src/main/java/com/example/wirle/parkeringsapp/ParkFragment.java
+++ app/src/main/java/com/example/wirle/parkeringsapp/ParkFragment.java
@@ -3,27 +3,57 @@ package com.example.wirle.parkeringsapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ParkFragment extends Fragment {
-
+public class ParkFragment extends Fragment implements OnMapReadyCallback {
 
     public ParkFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_park, container, false);
+        View v = inflater.inflate(R.layout.fragment_park, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                Log.d("GoogleMap", "Enter onMapReady");
+                LatLng sydney = new LatLng(-33.852, 151.211);
+                googleMap.addMarker(new MarkerOptions().position(sydney)
+                        .title("Marker in Sydney"));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            }
+        });
+
+        return v;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d("GoogleMap", "Enter onMapReady");
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
