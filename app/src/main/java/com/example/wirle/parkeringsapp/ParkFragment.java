0be05ee6fd6d046fc5f0f899953e07e334dc2f5e
johanbx <johan.borgenstierna@outlook.com>
2017-11-28 22:07:37 +0100

    nu crasher inte appen om gps är avstängd

--- app/src/main/java/com/example/wirle/parkeringsapp/ParkFragment.java
+++ app/src/main/java/com/example/wirle/parkeringsapp/ParkFragment.java
@@ -145,9 +145,13 @@ public class ParkFragment extends Fragment implements OnMapReadyCallback {
                         if (task.isSuccessful()) {
                             // Set the map's camera position to the current location of the device.
                             mLastKnownLocation = task.getResult();
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                            if (mLastKnownLocation != null)
+                            {
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
                                     new LatLng(mLastKnownLocation.getLatitude(),
-                                            mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                                        mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                            }
+
                         } else {
                             //Log.d(TAG, "Current location is null. Using defaults.");
                             //Log.e(TAG, "Exception: %s", task.getException());
