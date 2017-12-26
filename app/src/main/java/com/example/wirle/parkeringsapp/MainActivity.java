ed4c136e615486955a997fb8b82f67b2f8c47ba3
johanbx <johan.borgenstierna@outlook.com>
2017-12-26 16:39:42 +0100

    navigeringen läggs nu på backstack

--- app/src/main/java/com/example/wirle/parkeringsapp/MainActivity.java
+++ app/src/main/java/com/example/wirle/parkeringsapp/MainActivity.java
@@ -62,16 +62,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -183,7 +173,7 @@ public class MainActivity extends AppCompatActivity
             manager.beginTransaction().replace(
                     R.id.fragment_holder,
                     parkFragment
-            ).commit();
+            ).addToBackStack(null).commit();
         }
         else if (id == R.id.nav_about) {
             analyseNavigationSelect(Integer.toString(id), "nav about");
@@ -192,7 +182,7 @@ public class MainActivity extends AppCompatActivity
             manager.beginTransaction().replace(
                     R.id.fragment_holder,
                     aboutFragment
-            ).commit();
+            ).addToBackStack(null).commit();
         } else if (id == R.id.nav_buy) {
 
         } else if (id == R.id.nav_places) {
@@ -202,7 +192,7 @@ public class MainActivity extends AppCompatActivity
             manager.beginTransaction().replace(
                     R.id.fragment_holder,
                     placeFragment
-            ).commit();
+            ).addToBackStack(null).commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
